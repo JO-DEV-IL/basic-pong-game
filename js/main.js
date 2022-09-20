45ee2573f8842308bc36d4628c9574f74b9a4558
Jillian O'Keefe <jillianokeefe.dev@gmail.com>
2022-09-20 13:38:25 -0400

    complete

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
 // Update Loop
 import Ball from './ball.js'
+import Paddle from './paddle.js'
 
 const ball = new Ball(document.getElementById('ball'))
+const playerPaddle = new Paddle(document.getElementById('player-paddle'))
+const computerPaddle = new Paddle(document.getElementById('computer-paddle'))
+const playerScoreElem = document.getElementById('player-score')
+const computerScoreElem = document.getElementById('computer-score')
 
 // By passing time through the update function, the code will loop forever and therefore always update the screen
 let lastTime
 function update(time){
     if(lastTime != null){
         const delta = time - lastTime
-        ball.update(delta)
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
+        computerPaddle.update(delta, ball.y)
+        const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--hue'))
+
+        document.documentElement.style.setProperty('--hue', hue + delta * -0.01)
+
+        if(isLose()){
+            handleLose()
+        }
     }
     lastTime = time
     window.requestAnimationFrame(update)
 }
+
+function isLose(){
+    const rect = ball.rect()
+    return rect.right >= window.innerHeight || rect.left <= 0
+}
+
+function handleLose(){
+    const rect = ball.rect()
+    if(rect.right >= window.innerWidth){
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    }else{
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
 window.requestAnimationFrame(update)
+
+document.addEventListener('mousemove', e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
